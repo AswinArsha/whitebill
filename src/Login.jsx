545f481c722be6f7c4231bd9cca0cbddfac1dc28
aswinmv13 <aswinmv13@gmail.com>
2025-01-17 09:55:54 +0000

    removed remember me an updte package_lock.json

--- src/Login.jsx
+++ src/Login.jsx
@@ -194,21 +194,7 @@ const Login = ({ setRole, setIsAuthenticated, setUserId }) => {
               )}
             </div>
 
-            {/* Remember Me Checkbox */}
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-                  checked={rememberMe}
-                  onChange={(e) => setRememberMe(e.target.checked)}
-                />
-                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-700">
-                  Remember me
-                </label>
-              </div>
-            </div>
+      
 
             {/* Submit Button */}
             <Button
